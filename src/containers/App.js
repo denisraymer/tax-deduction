7906d343bd0bfac6c433f9e45f0b2f9e60bd8fde
denisraymer <to.mars.66.miles@gmail.com>
2020-11-07 04:20:18 +0300

    Added styles for the home screen

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function App() {
     return (
-        <h1>Налоговый вычет</h1>
+        <div className='container'>
+            <button className='button button-ghost'>Налоговый вычет</button>
+        </div>
     );
 }
 
