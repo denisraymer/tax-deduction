aa955f71a5b5e83e0d7f0a4f6b9d1eaf043dc13a
denisraymer <to.mars.66.miles@gmail.com>
2020-11-13 23:25:51 +0300

    Added gh-pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import {Page404} from '../page/Page404';
 import Main from '../page/Main';
 
